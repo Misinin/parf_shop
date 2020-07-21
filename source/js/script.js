33b5d9eae0a666ae448315f7cc6ea211a013b371
MisininM <mmisinin14@gmail.com>
2020-07-21 23:06:54 +0300

    Добавляет навигацию сайта

---
+++ source/js/script.js
@@ -0,0 +1,6 @@
+const navigation = document.querySelector('.nav');
+const navigationButton = document.querySelector('.nav__button');
+
+const translateNavigationX = () => {
+  navigation.classList.toggle('nav--active');
+}
